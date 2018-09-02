3170dcb38fe4e57b644f25abd9d3d1b884732211
Tomasz Cichoń <mrciastexx@gmail.com>
2018-09-02 11:04:12 +0200

    Listbox style now applies to default listbox control as well.

--- ListBox.cs
+++ ListBox.cs
@@ -4,9 +4,20 @@ namespace Atlas.UI
 {
     public class ListBox : System.Windows.Controls.ListBox
     {
+
         static ListBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ListBox), new FrameworkPropertyMetadata(typeof(ListBox)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ListBoxItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is ListBoxItem;
         }
     }
 }
--- ListBoxItem.cs
+++ ListBoxItem.cs
@@ -4,20 +4,6 @@ namespace Atlas.UI
 {
     public class ListBoxItem : System.Windows.Controls.ListBoxItem
     {
-        public static readonly DependencyProperty ShowRemoveButtonProperty = DependencyProperty.Register("ShowRemoveButton", typeof(bool), typeof(ListBoxItem));
-        public bool ShowRemoveButton
-        {
-            get { return (bool)GetValue(ShowRemoveButtonProperty); }
-            set { SetValue(ShowRemoveButtonProperty, value); }
-        }
-
-        public static readonly DependencyProperty ShowAddItemButtonProperty = DependencyProperty.Register("ShowAddItemButton", typeof(bool), typeof(ListBoxItem));
-        public bool ShowAddItemButton
-        {
-            get { return (bool)GetValue(ShowAddItemButtonProperty); }
-            set { SetValue(ShowRemoveButtonProperty, value); }
-        }
-
         static ListBoxItem()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ListBoxItem), new FrameworkPropertyMetadata(typeof(ListBoxItem)));
