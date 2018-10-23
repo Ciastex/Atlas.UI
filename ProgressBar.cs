fb6a6570d417f44605236def62c801b72fa8d457
Tomasz Cichoń <mrciastexx@gmail.com>
2018-10-23 22:43:48 +0200

    Got a progressbar, kinda.

---
+++ ProgressBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Atlas.UI
+{
+    public class ProgressBar : System.Windows.Controls.ProgressBar
+    {
+        static ProgressBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressBar), new FrameworkPropertyMetadata(typeof(ProgressBar)));
+        }
+    }
+}
