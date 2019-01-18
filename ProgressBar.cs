5fd9ff40055a0208442ae1ced3b445e43c127a1d
Ciastex <mrciastexx@gmail.com>
2019-01-18 14:20:59 +0100

    Added custom text templating in ProgressBar control.

--- ProgressBar.cs
+++ ProgressBar.cs
@@ -5,11 +5,18 @@ namespace Atlas.UI
     public class ProgressBar : System.Windows.Controls.ProgressBar
     {
         public static readonly DependencyProperty ShowProgressTextProperty = DependencyProperty.Register(nameof(ShowProgressText), typeof(bool), typeof(ProgressBar));
+        public static readonly DependencyProperty ProgressTextTemplateProperty = DependencyProperty.Register(nameof(ProgressTextTemplate), typeof(string), typeof(ProgressBar));
 
         public bool ShowProgressText
         {
             get => (bool)GetValue(ShowProgressTextProperty);
             set => SetValue(ShowProgressTextProperty, value);
+        }
+
+        public string ProgressTextTemplate
+        {
+            get => (string)GetValue(ProgressTextTemplateProperty);
+            set => SetValue(ProgressTextTemplateProperty, value);
         }
 
         static ProgressBar()
