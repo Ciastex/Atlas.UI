0554bc865698b0f00d9bd3d7813d80122f04d38e
Tomasz Cichoń <mrciastexx@gmail.com>
2019-01-23 20:41:08 +0100

    Added ListView control to the supported controls.

---
+++ Atlas.UI/ListViewItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Atlas.UI
+{
+    public class ListViewItem : System.Windows.Controls.ListViewItem
+    {
+        static ListViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ListViewItem), new FrameworkPropertyMetadata(typeof(ListViewItem)));
+        }
+    }
+}
