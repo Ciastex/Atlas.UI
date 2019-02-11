75b2880b99e3d5c743c767acd7207f345536c5c7
Tomasz Cichoń <mrciastexx@gmail.com>
2019-02-11 12:09:12 +0100

    Added tableview, textblock.

--- Atlas.UI/TableView.cs
+++ Atlas.UI/TableView.cs
@@ -1,6 +1,7 @@
 ﻿using Atlas.UI.Extensions;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
@@ -14,6 +15,7 @@ namespace Atlas.UI
         public static readonly DependencyProperty ItemsProperty = Dependency.Register<ObservableCollection<TableViewItem>>(nameof(Items));
         public static readonly DependencyProperty ItemTemplateProperty = Dependency.Register<DataTemplate>(nameof(ItemTemplate));
         public static readonly DependencyProperty UniformHeaderWidthProperty = Dependency.Register<double>(nameof(UniformHeaderWidth), true);
+        public static readonly DependencyProperty HasItemsProperty = Dependency.Register<bool>(nameof(HasItems));
 
         public ObservableCollection<TableViewItem> Items
         {
@@ -33,9 +35,17 @@ namespace Atlas.UI
             set => SetValue(UniformHeaderWidthProperty, value);
         }
 
+        public bool HasItems => (bool)GetValue(HasItemsProperty);
+
         static TableView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TableView), new FrameworkPropertyMetadata(typeof(TableView)));
+        }
+
+        public TableView()
+        {
+            Items = new ObservableCollection<TableViewItem>();
+            Items.CollectionChanged += (sender, e) => SetValue(HasItemsProperty, Items.Any());
         }
 
         public static void SetUniformHeaderWidth(UIElement element, double value)
@@ -46,11 +56,6 @@ namespace Atlas.UI
         public static double GetUniformHeaderWidth(UIElement element)
         {
             return (double)element.GetValue(UniformHeaderWidthProperty);
-        }
-
-        public TableView()
-        {
-            Items = new ObservableCollection<TableViewItem>();
         }
     }
 }
