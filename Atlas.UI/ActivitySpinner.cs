d2e4c3b57ef02ba6730e2a8210f54f1ed9b44d22
Tomasz Cichoń <mrciastexx@gmail.com>
2019-03-20 09:34:47 +0100

    Fix for issue #3

--- Atlas.UI/ActivitySpinner.cs
+++ Atlas.UI/ActivitySpinner.cs
@@ -1,11 +1,16 @@
 ﻿using Atlas.UI.Extensions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace Atlas.UI
 {
     public class ActivitySpinner : Control
     {
+        internal const string FadeInVisualStateName = "FadeIn";
+        internal const string FadeOutVisualStateName = "FadeOut";
+
         public static readonly DependencyProperty IsTaskRunningProperty = Dependency.Register<bool>(nameof(IsTaskRunning));
 
         public bool IsTaskRunning
@@ -17,6 +22,31 @@ namespace Atlas.UI
         static ActivitySpinner()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ActivitySpinner), new FrameworkPropertyMetadata(typeof(ActivitySpinner), null));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var storyboard = FindResource("SpinStoryboard") as Storyboard;
+            var path = GetTemplateChild("SpinnerElement") as Path;
+
+            path.BeginStoryboard(storyboard);
+
+            if (IsTaskRunning)
+                VisualStateManager.GoToState(this, FadeInVisualStateName, false);
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == nameof(IsTaskRunning))
+            {
+                if ((bool)e.NewValue)
+                    VisualStateManager.GoToState(this, FadeInVisualStateName, false);
+                else
+                    VisualStateManager.GoToState(this, FadeOutVisualStateName, false);
+            }
+            base.OnPropertyChanged(e);
         }
     }
 }
