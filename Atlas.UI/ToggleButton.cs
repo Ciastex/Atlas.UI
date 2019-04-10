d1b9958597aacae489cf869c763d748fcca0c433
Tomasz Cichoń <mrciastexx@gmail.com>
2019-04-10 10:44:49 +0200

    Added toggle button and corresponding image button...

---
+++ Atlas.UI/ToggleButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Atlas.UI
+{
+    public class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
+    {
+        static ToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleButton), new FrameworkPropertyMetadata(typeof(ToggleButton)));
+        }
+    }
+}
