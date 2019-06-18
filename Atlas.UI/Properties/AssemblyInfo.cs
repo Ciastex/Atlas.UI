266cc91b10662beb68b42ffbece676034e207156
Tomasz Cichoń <mrciastexx@gmail.com>
2019-06-18 10:54:36 +0200

    Updated .nuspec and assembly info for 0.7.6 release

--- Atlas.UI/Properties/AssemblyInfo.cs
+++ Atlas.UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Atlas.UI")]
-[assembly: AssemblyCopyright("Copyright © Ciastex 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Ciastex")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.5.*")]
-[assembly: AssemblyFileVersion("0.7.5.0")]
+[assembly: AssemblyVersion("0.7.6.*")]
+[assembly: AssemblyFileVersion("0.7.6.0")]
